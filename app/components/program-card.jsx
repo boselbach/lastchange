bcce7fe1aa99c4053d620760727d8d7641891125
Bo Skov Selbach <boselbach@gmail.com>
2017-05-30 09:12:43 +0200

    Adds duration on cards

--- app/components/program-card.jsx
+++ app/components/program-card.jsx
@@ -3,6 +3,16 @@ import React from 'react';
 export default class ProgramCard extends React.Component {
     constructor(props) {
         super(props);
+        console.log(props);
+
+        this.millisecondstoSeconds = this.millisecondstoSeconds.bind(this);
+    }
+
+    millisecondstoSeconds(milliseconds) {
+        let minutes = Math.floor(milliseconds / 60000);
+        let seconds = ((milliseconds % 60000) / 1000).toFixed(0);
+
+        return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
     }
 
     render() {
@@ -19,6 +29,7 @@ export default class ProgramCard extends React.Component {
                 <a href={card.PresentationUri} target="_blank">
                     <p className="title">{card.Title}</p>
                     <img src={card.PrimaryImageUri} alt=""/>
+                    <p className="duration">&#128336;&nbsp;{this.millisecondstoSeconds(card.PrimaryAsset.DurationInMilliseconds)}</p>
                 </a>
             </div>
         );
